cf663aa6563c9844d79949cbac801bfec24e3047
Jerry <sjerrys@gmail.com>
2012-03-27 00:50:31 +0800

    add daemon

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
+var daemon = require('daemon');
+
 require("coffee-script");
 require("tower").run(process.argv);
 
+
+daemon.daemonize('tmp/stdout-and-stderr.log', 'tmp/shop.pid', function (err, pid) {
+    //
+    // We are now in the daemon process
+    //
+    if (err) {
+      return console.log('Error starting daemon: ' + err);
+    }
+
+    console.log('Daemon started successfully with pid: ' + pid);
+  });
