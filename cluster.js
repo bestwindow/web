79169261faee89c72e7e52fe2a17af6f05ccf7aa
Jerry (孙晨) <sjerrys@gmail.com>
2012-07-23 22:30:20 +0800

    c

--- cluster.js
+++ cluster.js
@@ -7,14 +7,27 @@ require("coffee-script");
 
 if (cluster.isMaster) {
   // Fork workers.
+  var workers = []  
   for (var i = 0; i < numCPUs; i++) {
-    cluster.fork();
+    workers.push(cluster.fork());
   }
 
   cluster.on('death', function(worker) {
     console.log('worker ' + worker.pid + ' died');
   });
   console.log('Master PID:', process.pid);
+  
+  
+  process.on('SIGTERM', function() {
+    console.log('Master killed');
+  
+    workers.forEach(function(w) {
+      w.kill();
+    });
+  
+    process.exit(0);
+  });  
+  
 } else {
   console.log('Worker PID:', process.pid);
   require("tower").run(process.argv);
